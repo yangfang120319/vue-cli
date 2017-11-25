25a1bfbad4367711e062fc2167c53b5c4cc9e877
yangfang120319 <1203191871@qq.com>
2017-11-25 18:45:17 +0800

    second

--- work/vuecanvavedio/src/components/HelloWorld.vue
+++ work/vuecanvavedio/src/components/HelloWorld.vue
@@ -1,20 +1,14 @@
 <template>
   <div>
-	<mycomponent width="550" vediosrc="/static/media/mov_bbb.198918f.mp4"></mycomponent>
+	<mycomponent vediosrc="/src/assets/mov_bbb.mp4"></mycomponent>
   </div>
 </template>
 
 <script>
 import mycomponent from './mycomponent'
 
-
 export default {
   name: 'Canvas',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
-    }
-  },
   components: {
     mycomponent
   }
--- work/vuecanvavedio/src/components/mycomponent.vue
+++ work/vuecanvavedio/src/components/mycomponent.vue
@@ -1,61 +1,58 @@
 <template>
   <div>
 	<video loop controls autoplay  ref="mvideo" >
-		<source :src="vediosrc" type="video/mp4"></source>
+	  <source :src="vediosrc" type="video/mp4"></source>
 	</video>
-	<canvas @click="playPause" :width="width" :height="height" ref="Mycanvas">
-    </canvas>
+	<canvas @click="playPause" :width="width" :height="height" ref="mycanvas"></canvas>
   </div>
 </template>
 
 <script>
 
+
 export default {
-	props: {
-      title: {
-        type: String,
-        default: '正在载入...'
-      },
-	  vediosrc: {
-		type: String,
-        default: ''
-	  },
-	  width: {
-		type: String,
-        default: '600'
-	  },
-	  height: {
-		type: String,
-        default: '400'
-	  }
-    },
-	mounted() {
-		var c = this.$refs.Mycanvas
-		var myVideo = this.$refs.mvideo
-		var ctx = c.getContext('2d')
-		var that = this
-		myVideo.addEventListener('play', function() {
-			var i = window.setInterval(function() {
-				ctx.drawImage(myVideo, 0, 0, that.width, that.height)
-			}, 20);
-		}, false);
-		
-	  },
-	methods: {
-		playPause: function () {
-			if (this.$refs.mvideo.paused) 
-			  this.$refs.mvideo.play(); 
-			else 
-			  this.$refs.mvideo.pause(); 
-		},
+  props: {
+	vediosrc: {
+	  type: String,
+      default: ""
+	},
+	width: {
+	  type: Number,
+      default: 600
+	},
+	height: {
+	  type: Number,
+      default: 400
 	}
+  },
   
+  mounted() {
+	var c = this.$refs.mycanvas
+	var myVideo = this.$refs.mvideo
+	var ctx = c.getContext('2d')
+	var that = this
+	myVideo.addEventListener('play', function() {
+		var i = window.setInterval(function() {
+			ctx.drawImage(myVideo, 0, 0, that.width, that.height)
+		}, 20)
+	}, false)
+		
+  },
+  
+  methods: {
+	playPause: function () {
+	  if (this.$refs.mvideo.paused) 
+		this.$refs.mvideo.play() 
+	  else 
+		this.$refs.mvideo.pause()
+	},
+  } 
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-video{
-	display:none
+video {
+  display:none
 }
 </style>
